3f3456118f637c21529940fe9f25230192e8dda0
Rodolfo Campos <crodolfo@vmware.com>
2020-11-19 19:24:37 +0100

    Initial commit

---
+++ src/test/java/com/geeks/juber/checkout/CheckoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geeks.juber.checkout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
